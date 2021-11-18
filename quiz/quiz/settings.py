96cda3f1899366625801c7ae8c0838868188021c
Vaidehi9673 <61526327+Vaidehi9673@users.noreply.github.com>
2021-11-18 09:54:27 +0530

    Update settings.py

--- quiz/quiz/settings.py
+++ quiz/quiz/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#97%(z88-bf-_6rqpgo1jsledgbhat-c$ou10hcdco%p1sj9o8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
